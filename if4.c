27c23c2fb3e44ad88622bf001c9826cc67ba2045
aditya <adityanavadiya6790@gmail.com>
2025-08-25 15:39:54 +0530

    Add files via upload

---
+++ if4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a/7)
+    {
+    printf("given nomber is divisible by 7");
+    }
+    else{
+    printf("it is not divisible by7");
+    }
+}
