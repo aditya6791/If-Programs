6d4e5512111919b0c4a1072b70798ebc70674036
aditya <adityanavadiya6790@gmail.com>
2025-08-25 14:32:57 +0530

    Add files via upload

---
+++ if1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+
+scanf("%d %d",&a,&b);
+if(a>b)
+{
+ printf("the larger number is:%d",a);
+  }
+  else
+  {
+  printf("the larger number is:%d",b);
+  }
+}
---
+++ if2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+scanf("%d %d %d",&a,&b,&c);
+if(a>b)
+{
+d=a;
+}
+else
+    {
+        d=b;
+    }
+if(d>c)
+{
+
+    printf("the lagest digit is :%d",d);
+}
+else
+    {
+     printf("the lagest digit is :%d",c);
+}
+}
